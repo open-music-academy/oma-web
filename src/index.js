40317939471b41c3dfb0852c0b6b9767b21be680
Adriana Luchian <adriana.dumitras@gmail.de>
2022-01-05 10:53:48 +0100

    OMA-25 Update educandu to v0.11.0

--- src/index.js
+++ src/index.js
@@ -37,7 +37,8 @@ const config = {
     idlePollIntervalInMs: 10000,
     maxAttempts: 3
   },
-  additionalControllers: [ArticlesController]
+  additionalControllers: [ArticlesController],
+  areRoomsEnabled: processEnv.OMA_ARE_ROOMS_ENABLED
 };
 
 educandu(config);
