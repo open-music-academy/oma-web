46ae9ee22236f04e2e5e4f042346ac825e72441d
Andreas Helmberger <public@ahelmberger.de>
2022-02-10 16:57:43 +0100

    OMA-41 Use educandu dev tools

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/src/test-setup.js'],
-  transform: {
-    '\\.jsx?$': '@educandu/node-jsx-loader'
-  }
-};
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,6 +1,5 @@
-/* eslint-disable capitalized-comments */
-
 export const global = window;
 
+// eslint-disable-next-line capitalized-comments
 // midi-js (dep of abcjs) wants to have the global MIDI object:
 global.MIDI = {};
