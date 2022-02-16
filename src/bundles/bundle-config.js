7e9098b6e2d7b9c5c431575e39070832c561d55f
Adriana Luchian <adriana.dumitras@gmail.de>
2022-02-16 11:12:40 +0100

    OMA-44 Simplify site logo and introduce home page logo

--- src/bundles/bundle-config.js
+++ src/bundles/bundle-config.js
@@ -1,12 +1,13 @@
 import SiteLogo from './site-logo.js';
 import PageTemplate from './page-template.js';
+import HomePageLogo from './home-page-logo.js';
 
 export default {
   getPageTemplateComponent() {
     return PageTemplate;
   },
   getHomePageLogoComponent() {
-    return null;
+    return HomePageLogo;
   },
   getSiteLogoComponent() {
     return SiteLogo;
