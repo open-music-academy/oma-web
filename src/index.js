0f138e6cbef86d55eb85a384d498e15ed0c37be1
Adriana Luchian <adriana.dumitras@gmail.de>
2022-05-24 09:00:52 +0200

    OMA-76 Update educandu to v0.43.0

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ const config = {
   sessionCookieDomain: processEnv.OMA_SESSION_COOKIE_DOMAIN,
   sessionCookieName: processEnv.OMA_SESSION_COOKIE_NAME,
   consentCookieNamePrefix: processEnv.OMA_CONSENT_COOKIE_NAME_PREFIX,
+  uploadLiabilityCookieName: processEnv.OMA_UPLOAD_LIABILITY_COOKIE_NAME,
   emailSenderAddress: processEnv.OMA_EMAIL_SENDER_ADDRESS,
   smtpOptions: processEnv.OMA_SMTP_OPTIONS,
   initialUser: processEnv.OMA_INITIAL_USER ? JSON.parse(processEnv.OMA_INITIAL_USER) : null,
