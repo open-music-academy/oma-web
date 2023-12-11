ce053efc2bef7d6531044c3aebdab966b6036e65
Adriana Luchian <adriana.dumitras@gmail.de>
2023-12-11 17:24:44 +0100

    NOTICKET: Correct linting setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@educandu/eslint-config'],
+  extends: ['./.eslint-config.cjs'],
   rules: {
     'import/no-unresolved': ['error', { ignore: ['^@educandu/*', '^@benewagner/*'] }]
   }
